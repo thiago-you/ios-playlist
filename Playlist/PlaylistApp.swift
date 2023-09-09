95375a284336e90419f841ad7cdc15842f90c755
thiago-you <thiago.youx@gmail.com>
2023-09-09 19:18:57 -0400

    Initial Commit

---
+++ Playlist/PlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistApp.swift
+//  Playlist
+//
+//  Created by user241334 on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
