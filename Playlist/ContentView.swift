649abf4bbc25405e9bf2d314228880a9b173d772
thiago-you <thiago.youx@gmail.com>
2023-09-13 23:02:32 -0400

    implementing geometry reader

--- Playlist/ContentView.swift
+++ Playlist/ContentView.swift
@@ -10,81 +10,97 @@ import SwiftUI
 struct ContentView: View {
     let dataModel = DataModel()
     
+    func getOffsetY(reader: GeometryProxy) -> CGFloat {
+        let offsetY: CGFloat = -reader.frame(in: .named("scrollView")).minY
+        
+        if offsetY < 0 {
+            return offsetY / 1.3
+        }
+        
+        return offsetY
+    }
+    
     var body: some View {
-        GeometryReader { reader in
-            ScrollView {
+        ScrollView(showsIndicators: false) {
+            GeometryReader { reader in
+                let offsetY = getOffsetY(reader: reader)
+                
                 ZStack {
-                    VStack {
-                        Image("cover 1")
-                            .resizable()
-                            .frame(width: 200, height: 200)
-                        HStack {
-                            Button(action: {}) {
-                                Image(systemName: "arrow.down.circle.fill")
-                                    .foregroundColor(Color.black)
-                                    .padding(.trailing, 8)
+                    Image("cover 1")
+                        .resizable()
+                        .frame(width: reader.size.height, height: reader.size.height)
+                        .offset(y: offsetY)
+                }.frame(width: reader.size.width)
+            }
+            .frame(height: 200)
+                        VStack {
+                            HStack {
+                                Button(action: {}) {
+                                    Image(systemName: "arrow.down.circle.fill")
+                                        .foregroundColor(Color.black)
+                                        .padding(.trailing, 8)
+                                }
+                                Button(action: {}) {
+                                    Image(systemName: "person.fill.badge.plus")
+                                        .foregroundColor(Color.black)
+                                }
+                                Spacer()
+                                Button(action: {}) {
+                                    Image(systemName: "play.circle.fill")
+                                        .foregroundColor(Color.yellow)
+                                }
+                                .font(.largeTitle)
                             }
-                            Button(action: {}) {
-                                Image(systemName: "person.fill.badge.plus")
-                                    .foregroundColor(Color.black)
+                            .padding()
+                            VStack {
+                                Text("Playlist Eminem")
+                                    .font(.title)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.top, 5)
+                                    .padding(.bottom, 5)
+                                Text("Essa playlist é destinada aos melhores Jt. Leaners e Stans do Planeta ADDA!")
+                                    .fixedSize(horizontal: false, vertical: true)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.bottom, 10)
+                                HStack {
+                                    Image("profile")
+                                        .resizable()
+                                        .frame(width: 50, height: 30)
+                                        .clipShape(Circle())
+                                    Text("Playlist de Eminem")
+                                        .font(.subheadline)
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                                .padding(.leading, -10)
+                                .padding(.bottom, 10)
+                                ForEach(dataModel.covers) { cover in
+                                    HStack {
+                                        Image(cover.image)
+                                            .resizable()
+                                            .font(.largeTitle)
+                                            .bold()
+                                            .symbolRenderingMode(.monochrome)
+                                            .scaledToFit()
+                                            .frame(width: 50, height: 50)
+                                            .padding(.trailing, 10)
+                                            .padding(.bottom, 10)
+                                        VStack(alignment: .leading) {
+                                            Text(cover.title)
+                                                .font(.title2)
+                                                .bold()
+                                            Text(cover.description)
+                                        }
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .background(.white)
+                                }
                             }
-                            Spacer()
-                            Button(action: {}) {
-                                Image(systemName: "play.circle.fill")
-                                    .foregroundColor(Color.yellow)
-                            }
-                            .font(.largeTitle)
                         }
                         .padding()
-                        VStack {
-                            Text("Playlist Eminem")
-                                .font(.title)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.top, 5)
-                                .padding(.bottom, 5)
-                            Text("Essa playlist é destinada aos melhores Jt. Leaners e Stans do Planeta ADDA!")
-                                .fixedSize(horizontal: false, vertical: true)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .padding(.bottom, 10)
-                            HStack {
-                                Image("profile")
-                                    .resizable()
-                                    .frame(width: 50, height: 30)
-                                    .clipShape(Circle())
-                                Text("Playlist de Eminem")
-                                    .font(.subheadline)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                            .padding(.leading, -10)
-                            .padding(.bottom, 10)
-                            ForEach(dataModel.covers) { cover in
-                                HStack {
-                                    Image(cover.image)
-                                        .resizable()
-                                        .font(.largeTitle)
-                                        .bold()
-                                        .symbolRenderingMode(.monochrome)
-                                        .scaledToFit()
-                                        .frame(width: 50, height: 50)
-                                        .padding(.trailing, 10)
-                                        .padding(.bottom, 10)
-                                    VStack(alignment: .leading) {
-                                        Text(cover.title)
-                                            .font(.title2)
-                                            .bold()
-                                        Text(cover.description)
-                                    }
-                                }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .background(.white)
-                            }
-                        }
-                        Spacer()
+                        .background(Color.white)
                     }
-                    .padding()
-                }
-            }
-        }
+        
+        .coordinateSpace(name: "scrollView")
     }
 }
 
