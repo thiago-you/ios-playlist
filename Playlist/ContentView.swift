06679a71c131362c7d6f2e7b2e01d0a20bfe5b7c
thiago-you <thiago.youx@gmail.com>
2023-09-13 22:02:28 -0300

    adding header

--- Playlist/ContentView.swift
+++ Playlist/ContentView.swift
@@ -17,12 +17,35 @@ struct ContentView: View {
                         .frame(width: 250, height: 250)
                         .padding(.top, 20)
                     HStack {
-                        Image(systemName: "arrow.down.circle.fill")
-                        Image(systemName: "person.fill.badge.plus")
+                        Button(action: {}) {
+                            Image(systemName: "arrow.down.circle.fill")
+                                .foregroundColor(Color.black)
+                        }
+                        Button(action: {}) {
+                            Image(systemName: "person.fill.badge.plus")
+                                .foregroundColor(Color.black)
+                        }
                         Spacer()
-                        Image(systemName: "play.circle.fill")
+                        Button(action: {}) {
+                            Image(systemName: "play.circle.fill")
+                                .foregroundColor(Color.yellow)
+                        }
+                        .font(.title)
                     }
                     .padding()
+                    VStack {
+                        Text("Playlist Eminem")
+                            .font(.title)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.top, 20)
+                        Text("Essa playlist é destinada aos melhores Jt. Leaners e Stans do Planeta ADDA!")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        HStack {
+                            Image("profile")
+                            Text("Playlist de Eminem")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
                     Spacer()
                 }
                 .padding()
